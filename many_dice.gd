9fb04201dedcb91c7158954819b7cc37594f5527
OganM <ogan.mancarci@gmail.com>
2022-03-31 21:06:02 -0700

    sample outputs

--- many_dice.gd
+++ many_dice.gd
@@ -2,14 +2,16 @@ extends Control
 
 
 
-
+var rng = RandomNumberGenerator.new()
 var bar_graph = preload('utils/BarGraph/BarGraph.tscn')
 onready var bars = get_node("VBoxContainer/Output/Output/Bars")
 onready var dice = get_node("VBoxContainer/HBoxContainer/Dice")
 onready var explode_depth = get_node('VBoxContainer/HBoxContainer/VBoxContainer/ExplodeDepth')
 onready var mean = get_node("VBoxContainer/Output/Output/Mean")
+onready var sample_output = get_node("VBoxContainer/Output/Output/Sample_Out")
 
 func _ready():
+	rng.randomize()
 	pass # Replace with function body.
 
 
@@ -24,7 +26,18 @@ func _on_Calculate_pressed():
 	var output = dice_syntax.dice_probs(dice.text,depth)
 	var expected = dice_syntax.expected_value(output)
 	var old_bars = bars.get_node('BarContainer').get_children()
+	
+	var sample_out = 'Sample Output: '
+	for i in range(10):
+		if i !=9:
+			sample_out += str(dice_syntax.roll(dice.text,rng).result) + ', '
+		else:
+			sample_out += str(dice_syntax.roll(dice.text,rng).result)
+	print(sample_out)
+	
 	mean.text = 'Mean: '+str(expected)
+	sample_output.text = sample_out
+	
 	for x in old_bars:
 		x.queue_free()
 	
